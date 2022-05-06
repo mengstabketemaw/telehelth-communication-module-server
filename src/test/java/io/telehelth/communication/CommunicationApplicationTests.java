fab1df95b20d2e447fd812ba7d59eb385aaec570
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-06 16:29:14 -0700

    initial commit

---
+++ src/test/java/io/telehelth/communication/CommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.telehelth.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
