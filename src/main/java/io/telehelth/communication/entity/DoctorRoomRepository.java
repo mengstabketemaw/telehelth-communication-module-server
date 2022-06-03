414ade985b3c4628e73a636e135b8d74a7e77255
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-06-03 03:35:42 -0700

    add annotation "modifying" for the query deleteByUsername

--- src/main/java/io/telehelth/communication/entity/DoctorRoomRepository.java
+++ src/main/java/io/telehelth/communication/entity/DoctorRoomRepository.java
@@ -1,10 +1,12 @@
 package io.telehelth.communication.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface DoctorRoomRepository extends JpaRepository<DoctorRoom,Long> {
     Optional<DoctorRoom> findByUsername(String username);
+    @Modifying
     void deleteByUsername(String username);
 }
