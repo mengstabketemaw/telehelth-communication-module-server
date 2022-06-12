cfb6826032e72f37cb6f284f759b9d977aeff6a5
mengstabketemaw <mengstabrealk28@gmail.com>
2022-06-12 11:11:55 +0300

    added cross origine annotation

--- src/main/java/io/telehelth/communication/controller/WebSocketController.java
+++ src/main/java/io/telehelth/communication/controller/WebSocketController.java
@@ -6,10 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 
 @Controller
+@CrossOrigin
 public class WebSocketController {
     private final OnlineUsersService usersService;
 
